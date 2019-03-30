caa952d43c366d2b9a438ff51fd57356e68a77c0
captain-pool <rickdey1998@gmail.com>
2019-03-30 05:44:17 +0530

    Removed Dummy Server

--- serve.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request
-app = Flask(__name__)
-@app.route("/", methods = ["GET"])
-def index():
-  print(request.args)
-  return "YES"
-app.run("127.0.0.1",5000, debug = False)
